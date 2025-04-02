5edd12edb5adce2b59287c14b6e4424d5ec1739f
michael <yourgitemail@example.com>
2025-04-02 11:18:51 -0700

    pull-org.sh

--- nvim/lua/config/which-key.lua
+++ nvim/lua/config/which-key.lua
@@ -88,7 +88,8 @@ wk.register({
 			g = { ":Neogit<CR>","NeoGit"},
 		},
 		n = {
-			P = { ":!push-org.sh<CR>", "push org-roam"}
+			P = { ":!~/.config/nvim/scripts/push-org.sh<CR>", "push org-roam"},
+			p = { ":!~/.config/nvim/scripts/pull-org.sh<CR>", "pull org-roam"}
 		}
 
 	}

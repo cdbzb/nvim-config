a74a7dfee9779f4e47be747cf0769ceb1e9f902d
michael webster <you@example.com>
2025-07-07 15:48:21 -0700

    telescope action to delete buffer <C-d>

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,14 +1,19 @@
 
 require('telescope').setup{
+
   defaults = {
     -- Default configuration for telescope goes here:
     -- config_key = value,
     mappings = {
+      n = {
+    	  ['<c-d>'] = require('telescope.actions').delete_buffer
+      }, 
       i = {
         -- map actions.which_key to <C-h> (default: <C-/>)
         -- actions.which_key shows the mappings for your picker,
         -- e.g. git_{create, delete, ...}_branch for the git_branches picker
         ["<C-h>"] = "which_key",
+        ['<c-d>'] = require('telescope.actions').delete_buffer,
 		["<CR>"] = "select_default"
       }
     },
